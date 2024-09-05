91b6f8ec1eca73ba931190815226c916b2cea8b9
Dovday <dvdcarlomagno@gmail.com>
2024-09-05 15:59:23 +0200

    removed dependency to uuid

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import 'react-native-get-random-values';
-import { v4 as uuidv4 } from 'uuid';
+const generateId = (length) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
 
 export class SpatialHash {
   constructor(markers, categories) {
@@ -54,7 +64,7 @@ export class SpatialHash {
     }
 
     this.grid[x][y][category].push({
-      'uniqueId': uuidv4(),
+      'uniqueId': generateId(11),
       ...point
     });
   }
